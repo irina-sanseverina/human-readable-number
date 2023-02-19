7b918ba415e7006b9a7d776ee1bb1bdc8d9469ca
irina-sanseverina <irina.sanseverina@gmail.com>
2023-02-19 17:25:44 +0200

    feat: finish with the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function toReadable (number) {
-  
+	const ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+	const tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+ 
+	if (number === 0) return 'zero';
+	if (number < 20) return ones[number];
+	if (number < 100) return tens[Math.floor(number / 10)] + (number % 10 !== 0 ? ' ' + ones[number % 10] : '');
+	
+	if (number < 1000) {
+		if ((number % 100) === 0){
+			return ones[Math.floor(number / 100)] + ' hundred';
+		} else {
+			return ones[Math.floor(number / 100)] + ' hundred ' + toReadable(number % 100);
+		}
+	}
+		
+	if (number < 1000000) return toReadable(Math.floor(number / 1000)) + ' thousand ' + toReadable(number % 1000);
+	if (number < 1000000000) return toReadable(Math.floor(number / 1000000)) + ' million ' + toReadable(number % 1000000);
+	return toReadable(Math.floor(number / 1000000000)) + ' billion ' + toReadable(number % 1000000000);
 }
